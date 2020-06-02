a776e7b4fe52b6317cc9ea5d609aac8cc402eb80
Arrow <aronvinicius15@gmail.com>
2020-06-02 17:56:54 -0300

    Adicionando os Services

---
+++ RedisCache/Service/CacheService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using RedisCache.Interface;
+using System;
+
+namespace RedisCache.Service
+{
+    public class CacheService : ICacheService
+    {
+        public string GetCache(IDistributedCache cache, string key)
+        {
+            return cache.GetString(key);
+        }
+
+        public string SetCache(IDistributedCache cache, string key, string value)
+        {
+            DistributedCacheEntryOptions timeToLife =
+                          new DistributedCacheEntryOptions();
+
+            return cache.SetStringAsync(
+                key, 
+                value,
+                timeToLife.SetAbsoluteExpiration(TimeSpan.FromSeconds(30));
+        }
+    }
+}

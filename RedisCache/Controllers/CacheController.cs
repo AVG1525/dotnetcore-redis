ef4affc1dbafe990dcbbdd23b2bbbb6336367674
Arrow <aronvinicius15@gmail.com>
2020-06-02 17:57:24 -0300

    Adicionando CacheController

--- RedisCache/Controllers/CacheController.cs
+++ RedisCache/Controllers/CacheController.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using RedisCache.Interface;
 
 namespace RedisCache.Controllers
 {
     [Route("api/[controller]")]
     public class CacheController : BaseController
     {
-        [HttpGet]
-        public IActionResult Get()
+        private readonly ICacheService _cacheService;
+
+        [HttpGet("{key}")]
+        public IActionResult Get([FromServices]IDistributedCache cache, string key)
         {
-            return Ok("Okay");
+            return Ok(_cacheService.GetCache(cache, key));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromServices]IDistributedCache cache, string key, object value)
+        {
+            return Ok(_cacheService.SetCache(cache, key, value));
         }
     }
 }

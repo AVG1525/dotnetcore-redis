ff18ff9c8e84062ad784e5fcb030b9b0ae32cbbe
Arrow <aronvinicius15@gmail.com>
2020-06-02 17:16:58 -0300

    Adicionando intefaces

---
+++ RedisCache/Interface/ICacheService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisCache.Interface
+{
+    public interface ICacheService
+    {
+        string GetCache(IDistributedCache cache, string key);
+        string SetCache(IDistributedCache cache, string key, string value);
+    }
+}

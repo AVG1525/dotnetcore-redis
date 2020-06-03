25196bb93d78dcc4d0f30f7fae2398fc83a85b82
Arrow <aronvinicius15@gmail.com>
2020-06-03 11:20:12 -0300

    Finalizando Entitie

---
+++ RedisCache/Entitie/Request/CacheRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RedisCache.Entitie.Request
+{
+    public class CacheRequest
+    {
+        public string Key { get; private set; }
+        public string Values { get; private set; }
+        public double ExpirationTimeSeconds { get; private set; }
+    }
+}

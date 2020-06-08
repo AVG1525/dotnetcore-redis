7986d7460f94cf82dbf6fa2c98197d85972f7983
Arrow <aronvinicius15@gmail.com>
2020-06-08 09:53:23 -0300

    Retirando usings desnecessários

--- RedisCache/Startup.cs
+++ RedisCache/Startup.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using RedisCache.Interface;
 using RedisCache.Service;
 using StackExchange.Redis;
@@ -29,22 +21,9 @@ namespace RedisCache
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Serviço de cache sendo ativado por meioda chamada AddDistributedRedisCache 
-            //services.AddDistributedRedisCache(options =>
-            //{
-            //    options.Configuration = Configuration["ConexaoRedis"];
-            //    options.InstanceName = "master";
-            //});
-
             services.AddSingleton<IConnectionMultiplexer>(x =>
                 ConnectionMultiplexer.Connect(Configuration.GetValue<string>("ConexaoRedis")));
             services.AddSingleton<ICacheService, CacheService>();
-
-            //var ConnRedis = ConnectionMultiplexer.Connect(Configuration.GetConnectionString("ConexaoRedis"));
-
-
-            // Dependency Injection
-            services.AddTransient<ICacheService, CacheService>();
 
             services.AddControllers();
         }
